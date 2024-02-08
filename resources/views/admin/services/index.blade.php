65b231a9aebe6aa890b6ca4c5880f56bf09bcc98
webNekit <mochalov-hunter@mail.ru>
2024-02-08 18:39:54 +0300

    Добавили кнопку "Новая услуга" в шапку на странице

--- resources/views/admin/services/index.blade.php
+++ resources/views/admin/services/index.blade.php
@@ -2,9 +2,12 @@
 
 <x-app-layout>
     <x-slot:header>
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Услуги') }}
-        </h2>
+        <div class="w-full flex items-center justify-between">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('Услуги') }}
+            </h2>
+            <a href="{{ route('services.create') }}" class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">{{ __('Новая услуга') }}</a>
+        </div>
    </x-slot>
 
   <x-splade-table :for="$services" />
