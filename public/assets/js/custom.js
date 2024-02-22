7ea61b9cc3e677b465d33637fc383421fc850b61
webNekit <mochalov-hunter@mail.ru>
2024-02-22 11:15:46 +0300

    Сделали работающий слайдер

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -1,6 +1,6 @@
 
   (function ($) {
-  
+
   "use strict";
 
     // COUNTER NUMBERS
@@ -10,30 +10,30 @@
 
     // BACKSTRETCH SLIDESHOW
     $('.hero-section').backstretch([
-      "images/slideshow/afro-woman-cleaning-window-with-rag-home.jpg", 
-      "images/slideshow/afro-woman-holding-bucket-with-cleaning-items.jpg",
-      "images/slideshow/unrecognizable-cleaner-walking-into-hotel-room-with-tools-detergents.jpg"
+      "/assets/images/slideshow/afro-woman-cleaning-window-with-rag-home.jpg",
+      "/assets/images/slideshow/afro-woman-holding-bucket-with-cleaning-items.jpg",
+      "/assets/images/slideshow/unrecognizable-cleaner-walking-into-hotel-room-with-tools-detergents.jpg"
     ],  {duration: 2000, fade: 750});
-    
+
     // CUSTOM LINK
     $('.smoothscroll').click(function(){
       var el = $(this).attr('href');
       var elWrapped = $(el);
-  
+
       scrollToDiv(elWrapped);
       return false;
-  
+
       function scrollToDiv(element){
         var offset = element.offset();
         var offsetTop = offset.top;
         var totalScroll = offsetTop-navheight;
-  
+
         $('body,html').animate({
         scrollTop: totalScroll
         }, 300);
       }
     });
-    
+
   })(window.jQuery);
 
 
